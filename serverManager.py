fd3a7ed79e6cffddff8e75246d44365b1dd472df
Cooper Parlee <cooper@cooperparlee.com>
2025-08-14 16:23:23 -0400

    turned on garbage collection

--- serverManager.py
+++ serverManager.py
@@ -1,7 +1,7 @@
 import schedule
 import time
 
-from servStatusMessage import csvUpdateWrapper
+from servStatusMessage import csvUpdateWrapper, collectGarbage
 from dailySystemEmail import dailyEmail
 
 print("Starting Server Manager...")
@@ -9,6 +9,7 @@ print("Starting Server Manager...")
 def main():
     schedule.every().minute.do(csvUpdateWrapper)
     schedule.every().day.at("05:30").do(dailyEmail)
+    schedule.every().day.at("00:15").do(collectGarbage)
 
     while(True):
         schedule.run_pending()
